3c4b1d349b5ac94c3d4ec4bed53cf96c0509a49f
shyambhogle <shyambhongle@gmail.com>
2019-07-29 11:29:59 +0530

    test1

--- server.js
+++ server.js
@@ -56,16 +56,8 @@ app.use('/profile',profile);
 
 
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, "client/build")))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server Running on port ${port}`));
